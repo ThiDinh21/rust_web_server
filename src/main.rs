6ba2c36ca8356ca7ab13a8e374f3d5ddec3e35f3
Thi Dinh <vietthidinh2001@gmail.com>
2022-04-20 02:07:17 +0300

    feat: build a simple single-threaded web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::{
+    fs,
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get_req = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get_req) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let resp = format!(
+        "{status_line}\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents,
+    );
+
+    stream.write(resp.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
